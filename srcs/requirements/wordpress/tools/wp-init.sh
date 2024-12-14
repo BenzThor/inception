64d7eda6a16896f1813f8f44e787ded7c9aa6082
BenzThor <benzthorben@gmail.com>
2024-12-14 18:46:04 +0100

    changed the Dockerfiles of every container to have less layers; added a wordpress container and configuration

---
+++ srcs/requirements/wordpress/tools/wp-init.sh
@@ -0,0 +1,12 @@
+#!bin/sh
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+	wp core download --path=/var/www/html
+	wp config create --dbname=$DB_NAME_WP --dbuser=$DB_USER_WP --dbpass=$DB_PASSWORD_WP --dbhost=$DB_HOST_WP --path=/var/www/html
+	wp core install --url=$URL_WP --title=$TITLE_WP --admin_user=$ADMIN_USER_WP --admin_password=$ADMIN_PAASSWORD_WP --admin_email=$ADMIN_EMAIL_WP --path=/var/www/html
+	wp theme install oceanwp --activate --path=/var/www/html
+fi
+
+wp plugin update --all
+
+exec php-fpm82 --nodaemonize
