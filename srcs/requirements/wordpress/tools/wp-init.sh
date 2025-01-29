baa0b16ffebc04c0a7ee378a49446a0b641255b2
Thorben Benz <benzthorben@gmail.com>
2025-01-29 16:17:07 +0100

    changed workdir in wordpress Dockerfile, added additional logic for permissions

--- srcs/requirements/wordpress/tools/wp-init.sh
+++ srcs/requirements/wordpress/tools/wp-init.sh
@@ -15,13 +15,14 @@ echo $DB_PASSWORD_WP
 echo $DB_HOST_WP
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-	wp core download --path=/var/www/html
+	# wp core download --path=/var/www/html/
 	wait_for_maria_db
-	wp config create --dbname=$DB_NAME_WP --dbuser=$DB_USER_WP --dbpass=$DB_PASSWORD_WP --dbhost=$DB_HOST_WP --path=/var/www/html
-	wp core install --url=$URL_WP --title=$TITLE_WP --admin_user=$ADMIN_USER_WP --admin_password=$ADMIN_PASSWORD_WP --admin_email=$ADMIN_EMAIL_WP --path=/var/www/html
-	wp theme install oceanwp --activate --path=/var/www/html
+	wp config create --dbname="$DB_NAME_WP" --dbuser="$DB_USER_WP" --dbpass="$DB_PASSWORD_WP" --dbhost="$DB_HOST_WP"
+	wp core install --url="$URL_WP" --title="$TITLE_WP" --admin_user="$ADMIN_USER_WP" --admin_password="$ADMIN_PASSWORD_WP" --admin_email="$ADMIN_EMAIL_WP"
+
+	wp theme install oceanwp --activate
 fi
 
-wp plugin update --all
+# wp plugin update --all
 
 exec php-fpm --nodaemonize
